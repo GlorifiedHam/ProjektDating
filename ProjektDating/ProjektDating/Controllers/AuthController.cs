fbe32d1b95f7edb6aaa57042b969906f03b2afd0
nicsuv141 <nicsuv141@studentmail.oru.se>
2016-12-27 16:42:02 +0100

    fdg

---
+++ ProjektDating/ProjektDating/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjektDating.Models;
+using System.Security.Claims;
+
+namespace ProjektDating.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        [HttpGet]
+        public ActionResult Login()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Login(Users model)
+        {
+            if (!ModelState.IsValid) //Checks if input fields have the correct format
+            {
+                return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+            }
+
+            //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
+            if (model.Email == "admin@admin.com" && model.Password == "123456")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "Xtian"),
+                new Claim(ClaimTypes.Email, "xtian@email.com"),
+                new Claim(ClaimTypes.Country, "Philippines")
+            }, 
+                "ApplicationCookie");
+
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+            authManager.SignIn(identity);
+
+            return RedirectToAction("Index", "Home");
+        }
+            ModelState.AddModelError("", "Invalid email or password");
+        return View(model);
+    }
+        
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Login", "Auth");
+        }
+    }
+
+    
+}
---
+++ ProjektDating/ProjektDating/MainDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ProjektDating.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjektDating
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
